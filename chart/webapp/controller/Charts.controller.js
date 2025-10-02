1102811e99f2a38c990da1b39f08dcd155396cf6
DaffaRomyz <daffaromyzaufa@gmail.com>
2025-10-02 16:27:08 +0700

    Implement line chart

--- chart/webapp/controller/Charts.controller.js
+++ chart/webapp/controller/Charts.controller.js
@@ -1,10 +1,15 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/viz/ui5/format/ChartFormatter",
+    "sap/viz/ui5/api/env/Format",
+  ],
+  (Controller, ChartFormatter, Format) => {
     "use strict";
 
-    return Controller.extend("chart.controller.Charts", {
-        onInit() {
-        }
+    return Controller.extend("fiori.chart.controller.Charts", {
+      onInit() {
+      },
     });
-});
+  }
+);
